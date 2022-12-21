ba8a54dfeb880a273510bbcc8d132df32e4a741d
htetsoesan <htetsoesan@brycenmyanmar.com.mm>
2022-12-21 16:34:50 +0630

    initial commit

---
+++ app/src/main/java/com/padc/kotlin/ftc/helloviews/AppCompactComponentUsageActivity.kt
@@ -0,0 +1,12 @@
+package com.padc.kotlin.ftc.helloviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppCompactComponentUsageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_compact_component_usage)
+
+    }
+}
---
+++ app/src/main/java/com/padc/kotlin/ftc/helloviews/RelativeAndLinearLayoutAssignment.kt
@@ -0,0 +1,11 @@
+package com.padc.kotlin.ftc.helloviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeAndLinearLayoutAssignment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_aand_linear_layout_assignment)
+    }
+}
